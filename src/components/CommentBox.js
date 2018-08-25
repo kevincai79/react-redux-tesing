28fce9cba9a658b8cbdc85d1b96721edfc4a5ca5
Kevin Cai <kevincai1979@gmail.com>
2018-08-25 15:09:00 -0700

    implement fetchComments feature

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -19,16 +19,19 @@ class CommentBox extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h4>Add a Comment</h4>
-        <textarea
-          value={this.state.comment}
-          onChange={this.handleChange.bind(this)}
-        />
-        <div>
-          <button>Submit Comment</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea
+            value={this.state.comment}
+            onChange={this.handleChange.bind(this)}
+          />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button onClick={this.props.fetchComments}> Fetch Comment</button>
+      </div>
     );
   }
 }
